f5d397ef604324c0ddecee12bc7bee569947872b
Ashu <ashu.ask.singh@gmail.com>
2022-07-10 16:51:35 +0530

    add JWTTokenProvider

--- src/main/java/com/supportportal/utility/JWTTokenProvider.java
+++ src/main/java/com/supportportal/utility/JWTTokenProvider.java
@@ -5,10 +5,16 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.supportportal.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +24,7 @@ import java.util.stream.Collectors;
 
 import static com.supportportal.sonstant.SecurityConstant.*;
 
+@Component
 public class JWTTokenProvider {
 
     @Value("${jwt.secret}")
@@ -63,5 +70,27 @@ public class JWTTokenProvider {
             authorities.add(grantedAuthority.getAuthority());
         }
         return authorities.toArray(new String[0]);
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken userPasswordToken =
+                new UsernamePasswordAuthenticationToken(username, null, authorities);
+        userPasswordToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return userPasswordToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier, token);
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
     }
 }
